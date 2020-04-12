d81b0c01533f516b55cf3c4ec83f0e07fd8ae697
陈贲 <chenben@chenbendeMacBook-Pro.local>
2020-04-12 18:44:23 +0800

    add hanoi algo

--- hanoi.cpp
+++ hanoi.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 void hanoi(int n, char A,char B,char C){
   if(n==1){
-    cout<<A<<" -> "<<C<<endl;
+    cout<<A<<" -> "<<C<<endl;//move A to C
   } else{
-    hanoi(n-1,A,C,B);
-    cout<<A<<" -> "<<C<<endl;
-    hanoi(n-1,B,A,C);
+    hanoi(n-1,A,C,B);// move A to B, using C
+    cout<<A<<" -> "<<C<<endl;//then move A to C
+    hanoi(n-1,B,A,C);//move B to C, using A
   }
 }
 
 int main(){
-  int n;cin>>n;
-  hanoi(n,'A','B','C');
-
-  return 0;
+    int n;
+    cin>>n;
+    hanoi(n,'A','B','C');
+    return 0;
 }
